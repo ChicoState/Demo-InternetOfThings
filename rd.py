7c5dfe49b532e0599397a55d4f00383dc3b098b5
csilva25 <csilva25@mail.csuchico.edu>
2018-01-16 22:06:13 -0800

    adding comments to rd.py

--- rd.py
+++ rd.py
@@ -1,26 +1,26 @@
-from time import sleep
-import RPi.GPIO as GPIO
+from time import sleep #timing libraries
+import RPi.GPIO as GPIO #including the libraries for the "general" raspberry pi pins
 
-light = 17
+light = 17 #init the var "light" to the 17th gpio pin on raspberry pi
 
 from flask import (
 	Flask,
-	render_template,
-	request,
+    render_template, #will look for an html file
+    request, #contains data that browser has sent to the app
 )
 
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BCM) #initialling pins by the "Broadcom SOC channel"
 
-GPIO.setup(17, GPIO.Out)
+GPIO.setup(17, GPIO.OUT)  #setting pin as an output
 
-app = Flask(__name__)
+app = Flask(__name__) #use name if using a single module and something else if importing a module
 
-@app.route('/lights_on')
+@app.route('/lights_on') #"/" means root of the website (rbp ip) followed by "lights_on" page
 
-def setLights():
-	GPIO.output(light, GPIO.HIGH)
+def setLights(): #"setLights" is what we name the route for further reference
+    GPIO.output(light, GPIO.HIGH) #setting pin as an output, pulling voltage up (high), to the pin light (17)
 
-return 1
+    return 1 #end app
 	
-if __name__ == "__main__":
-	app.run('0.0.0.0', 5300)
+if __name__ == "__main__": #sets built in varaible name to main
+    app.run('0.0.0.0', 5300) #the 0.0.0.0 means this app is accessible by any device on the network
